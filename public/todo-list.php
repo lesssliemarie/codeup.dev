eb1ecd879e3fd2d4d82e7b0ad1643b6588c1dd35
lesssliemarie <lesssliemarie@gmail.com>
2014-02-25 13:29:08 -0600

    Added check for file type of plain text

--- public/todo-list.php
+++ public/todo-list.php
@@ -44,14 +44,14 @@ if (!empty($_GET['remove'])) {
 	exit();
 }
 
-if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0 && $_FILES['file1']['type'] == 'text/plain') {
 	$upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
 	$filename = basename($_FILES['file1']['name']);
 	$saved_filename = $upload_dir . $filename;
 	move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
 	$fileContents = read_file($saved_filename);
 	$items = array_merge($items, $fileContents);
-	save_file($file, $items);
+	save_file($file, $items);		
 	header("Location: todo-list.php");
 	exit(0);
 }
