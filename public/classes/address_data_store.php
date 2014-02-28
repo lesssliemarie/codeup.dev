a088035a00880c9ca39c28a6dd3a8c938790fc2c
lesssliemarie <lesssliemarie@gmail.com>
2014-02-28 15:53:18 -0600

    Used parent construct, override with strtolower

--- public/classes/address_data_store.php
+++ public/classes/address_data_store.php
@@ -3,6 +3,11 @@ require_once('filestore.php');
 // create class to read and write csv files
 class AddressDataStore extends Filestore {
 
+    function __construct($filename = '')
+    {
+        parent::__construct(strtolower($filename));       
+    }
+
     function readBook()
     {
         $contents = $this->readCSV($this->filename);
