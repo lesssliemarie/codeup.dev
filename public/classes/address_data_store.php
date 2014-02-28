368663ad10bafe9ee137affbef6f0c180ebccdbd
lesssliemarie <lesssliemarie@gmail.com>
2014-02-28 11:31:31 -0600

    Made child of Filestore and refactored functions

--- public/classes/address_data_store.php
+++ public/classes/address_data_store.php
@@ -1,34 +1,17 @@
 <?php 
+require_once('filestore.php');
 // create class to read and write csv files
-class AddressDataStore {
+class AddressDataStore extends Filestore {
 
-    public $filename = '';
-
-    function __construct($filename = 'data/address_book.csv') 
+    function readBook()
     {
-    	$this->filename = $filename;
+        $contents = $this->readCSV($this->filename);
+        return $contents;
     }
 
-    function readCSV()
+    function saveBook($contents) 
     {
-        // read file $this->filename
-        $contents = [];
-		$handle = fopen($this->filename, 'r');
-		while (($data = fgetcsv($handle)) !== FALSE) {
-			$contents[] = $data;
-		}
-		fclose($handle);
-		return $contents;
-    }
-
-    function saveCSV($contents) 
-    {
-        // write $contents array to file $this->filename
-        $handle = fopen($this->filename, 'w+');
-		foreach ($contents as $fields) {
-		fputcsv($handle, $fields);	
-		}
-		fclose($handle);
+        $this->saveCSV($contents);
     }
 
 }
