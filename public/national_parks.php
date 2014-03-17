450ed9e0556ea984c7010d732b31b1c0a564129d
lesssliemarie <lesssliemarie@gmail.com>
2014-03-17 10:31:01 -0500

    Removed unnecessary else

--- public/national_parks.php
+++ public/national_parks.php
@@ -6,10 +6,7 @@ $mysqli = @new mysqli('127.0.0.1', 'queenbee', 'b8z92u9z', 'codeup_mysqli_test_d
 // Check for errors
 if ($mysqli->connect_errno) {
 	echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error . PHP_EOL;
-} else {
-	// echo $mysqli->host_info . PHP_EOL;
-}
-
+} 
 
 // Retrieve a result set using SELECT
 
